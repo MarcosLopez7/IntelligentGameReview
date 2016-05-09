343904b06b6a925ad7a9d164df5759c11243d2ed
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-05-09 15:58:25 -0500

    a ver si si 2.0

--- reviews/views.py
+++ reviews/views.py
@@ -10,8 +10,12 @@ def list_game(request):
     texto = request.GET['texto']
     games = Nombre.objects.filter(nombre__icontains=texto)
     reviews = []
+    fotos = []
+    i = 0
     for game in games:
         reviews.append(General.objects.filter(nombre = game))
+        fotos.append(reviews[i].foto[7:])
+        i = i + 1
 
-    return render(request, 'reviews/results.html', {'reviews': reviews})
+    return render(request, 'reviews/results.html', {'reviews': reviews, 'fotos': fotos})
 
